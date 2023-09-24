4e72cbfaacdcd04fb214a4bf01df5c9f1c97af81
Delaney Green <delaney.rosa1@gmail.com>
2023-09-24 17:38:19 -0500

    Building blocks to user registration and login. Moving fast and hoping for the best.

---
+++ src/main/java/com/greenhouse/Thymeleaflinks/controllers/AuthenticationController.java
@@ -0,0 +1,46 @@
+package com.greenhouse.Thymeleaflinks.controllers;
+
+import com.greenhouse.Thymeleaflinks.data.UserRepository;
+import com.greenhouse.Thymeleaflinks.models.User;
+import com.greenhouse.Thymeleaflinks.models.dto.RegisterFormDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Controller
+public class AuthenticationController {
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    @GetMapping("/register")
+    public String displayRegistrationForm(Model model) {
+        model.addAttribute(new RegisterFormDTO());
+        model.addAttribute("title", "Register");
+        return "register";
+    }
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
+}
