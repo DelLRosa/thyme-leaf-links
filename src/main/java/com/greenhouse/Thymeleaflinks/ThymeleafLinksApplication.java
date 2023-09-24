0162c4d60f75afb96a7714a7e4e5b9b61fe649d6
Delaney Green <delaney.rosa1@gmail.com>
2023-09-24 15:37:28 -0500

    Initial Commit

---
+++ src/main/java/com/greenhouse/Thymeleaflinks/ThymeleafLinksApplication.java
@@ -0,0 +1,13 @@
+package com.greenhouse.Thymeleaflinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafLinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenhouse/Thymeleaflinks/ThymeleafLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenhouse.Thymeleaflinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafLinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
